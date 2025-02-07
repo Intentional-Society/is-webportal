4001ac144df05e588aa3d7905ebc7c38e909df78
James Baker <james-baker@users.noreply.github.com>
2025-02-07 11:35:09 -0800

    Q1 practice dojo at Limicon

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,10 +1,13 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The topic and timing of the Q1 Practice Dojo series are TBD and will be announced by mid-January. 
-  Sessions are usually on <b>Saturdays from 
-  1:00 to 2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. 
-  {/* <a 
+  Join us at <a href="https://www.limicon2025.com">Limicon 2025</a>! <br />
+  We're bringing the Practice Dojo into the convention and hosting topical fishbowl dialogues 
+  throughout the four-week span between March 5 to April 2. <br />
+  Regular IS Practice Dojo activities will resume Q2 2025.
+  
+  {/* Sessions are usually on <b>Saturdays from 
+  1:00 to 2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. <a 
   href="https://us02web.zoom.us/meeting/register/tZIvdOCtpj4qG9XxuaHMOq_7eGs_6dPCwhae">Click 
   here to register and receive the meeting link.</a> */}
 </>
