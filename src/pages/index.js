615a559feca8a93ace531f44faa38619d838e57d
James Baker <james-baker@users.noreply.github.com>
2025-03-04 22:56:26 -0800

    create hub page
    and continue trimming home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,57 +26,34 @@ const NamedDefault = ({ data }) => <>
         <p style={{margin:"1vw auto 1vw 3vw"}}>being who we want to be</p>
       </Typography>
     </div>
-    <div style={{margin: "0 auto", minWidth: "300px", maxWidth: "960px", padding: "0 50px", fontSize: "1.25em"}}>
-      <div style={{margin: "0 auto", minWidth: "300px", maxWidth: "max-content", padding: "0"}}>
-        <p>We are a congregation of friends devoted to growing ourselves 
-        and our society toward better futures. The complexity of our world 
-        is calling us to become…</p>
-      </div>
-      <div style={{margin: "0 auto", minWidth: "300px", maxWidth: "max-content", padding: "0"}}>
-        <p>perceptive enough to see deeper</p>
-        <p>big enough to be with anything</p>
-        <p>wise enough to wield our power</p>
-      </div>
-          
+    <div style={{margin: "0 auto", minWidth: "300px", maxWidth: "960px", padding: "0 50px"}}>
+      <div style={{fontSize: "1.25em"}}>
+        <div style={{margin: "0 auto", minWidth: "300px", maxWidth: "max-content", padding: "0"}}>
+          <p>We are a congregation of friends devoted to growing ourselves 
+          and our society toward better futures. The complexity of our world 
+          is calling us to become…</p>
+        </div>
+        <div style={{margin: "0 auto", minWidth: "300px", maxWidth: "max-content", padding: "0"}}>
+          <p>perceptive enough to see deeper</p>
+          <p>big enough to be with anything</p>
+          <p>wise enough to wield our power</p>
+        </div>
+        <IsHr />
+        <p>Being who we want to be is an ever-unfolding process, and we do it by practicing together. 
+          We consciously cultivate a post-conventional culture among us that shifts our seeing, 
+          supports our becoming, flows into our doing.</p>
+        <p>If you’ve been seeking the intersection of inner work and systems change, we 
+          invite you to join us in the fractal dance of transformation and in the 
+          friendship of collaborative service to life.</p>
+        </div>
 
       <IsHr />
-      <p>Being who we want to be is an ever-unfolding process, and we do it by practicing together. 
-        We consciously cultivate a post-conventional culture among us that shifts our seeing, 
-        supports our becoming, flows into our doing.</p>
 
-      <p>If you’ve been seeking the intersection of inner work and systems change, we 
-        invite you to join us in the fractal dance of transformation and in the 
-        friendship of collaborative service to life.</p>
-
-
-        <img src="/images/s17-3spaces.jpg" alt="3spaces"
-  style={{ display: "block", margin: "40px auto 10px auto", maxWidth: "50%" }} />
-
-
-
-      <Typography variant="h4">IS Community Hub (members collective)</Typography>
-
-      <p>Community members gather on Sundays for weekly calls, and/or support one another in relational "pods", 
-        while forming and deepening relationships with other community members. We play together in a relational 
-        sandbox of practices, explorations, creativity, and leadership development as we cultivate the kind of 
-        collective culture that supports us in our "being who we want to be" into the whole of our lives.</p>
-      
-      <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSebSp8vQsfRINQhHGw83Z-AEL3CtDtdcB5Edyc4kdCAq03XEg/viewform">Read 
-        more and apply here.</a> Membership is free (always has been), and the 
-        light application-and-agreements membrane that defines membership exists to let us 
-        create openness and safety in our private cozyweb and manage our private data sharing. 
-        You're invited to attend an <Link to="/get-involved#office-hours">Office Hours</Link> call 
-        for orientation purposes, yet if you've been to at least one Practice Dojo session previously,
-        you probably have enough of the vibe to dive in if you prefer.</p>
-
-      <Typography variant="h4">Intentional Ventures (private experiment)</Typography>
-
-      <p>We unlock successful economics for meaningful livelihoods through relational support.</p>
-
-      <p>After three quarters with its founding team, IV has made its first revenue, established 
-        key structures and principles, and added its first new participants. 
-        To be added to the waitlist, come chat about it at <Link to="/get-involved#office-hours">Office Hours</Link> or 
-        talk to any participant.</p>
+      <div style={{ width: "fit-content", margin: "0 auto"}}>
+        <StaticImage src="../../static/images/s17-3spaces.jpg"
+          loading="eager" alt="three spaces of IS: practice dojo, community hub, intentional ventures"
+          width="480" style={{margin: "40px auto 10px auto"}}/>
+      </div>
 
       <br />
       <IsHr />
--- src/pages/iv.js
+++ src/pages/iv.js
@@ -9,6 +9,14 @@ const NamedDefault = ({ data }) => (
     <div style={{ height: "5vh" }}></div>
     <CenteredColumn>
       <Typography variant="h2">Intentional Ventures</Typography>
+
+            <p>We unlock successful economics for meaningful livelihoods through relational support.</p>
+      
+            <p>After three quarters with its founding team, IV has made its first revenue, established 
+              key structures and principles, and added its first new participants. 
+              To be added to the waitlist, come chat about it at <Link to="/get-involved#office-hours">Office Hours</Link> or 
+              talk to any participant.</p>
+              
       <p>New as of April 2024. We intend to grow a thriving economic ecosystem that supports our material security,
         in ways that align with our values and purposes, leveraging a crucial foundation 
         of professional, relational, and developmental skills. Intentional Ventures is in 
