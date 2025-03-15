39a2d8ffa855744e86c5a64648ecc91fb0f1a6c9
James Baker <james-baker@users.noreply.github.com>
2025-03-14 18:10:36 -0700

    another design pass through homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,46 +55,47 @@ const NamedDefault = ({ data }) => <>
       <Typography variant="h2" style={{ color: "#FFFFFF"}}>
         <p style={{margin:"1vw auto"}}>Intentional Society</p>
       </Typography>
-      <Typography variant="h5" style={{ color: "#FFFFFF"}}>
-        <p style={{marginLeft:"3vw"}}>starts with...</p>
+      <Typography variant="h4" style={{ color: "#FFFFFF"}}>
+        <p style={{margin:"1vw auto 1vw 3vw"}}>starts with...</p>
       </Typography>
       <Typography variant="h4" style={{ color: "#FFFFFF"}}>
         <p style={{margin:"1vw auto 1vw 3vw"}}>being who we want to be</p>
       </Typography>
     </div>
     <CenteredColumn>
-      <div style={{fontSize: "1.25em"}}>
+      <div style={{fontSize: "1.2em"}}>
 
           <p>We are a congregation of friends devoted to growing ourselves 
-          and our society toward better futures. The complexity of our world 
+          and our society toward thriving futures. 
+          In these complex and chaotic times, our world 
           is calling us to become&hellip;</p>
 
-        <div style={{margin: "0 auto 0 100px"}}>
-          <p>&hellip;<b>sensitive</b> enough to <b>see</b> clearly</p>
-          <p>&hellip;<b>flexible</b> enough to <b>be with</b> anything</p>
-          <p>&hellip;and <b>wise</b> enough to wield our <b>power</b></p>
+        <div style={{margin: "0 auto 0 100px", fontSize: "1.5em"}}>
+          <p>&hellip;<b>aware</b> enough to <b>see</b> clearly 👁‍🗨</p>
+          <p>&hellip;<b>spacious</b> enough to <b>face</b> anything 💗</p>
+          <p>&hellip;and <b>wise</b> enough to <b>wield</b> our power 👑</p>
         </div>
         <IsHr />
-        <p>Being who we want to be is an ever-unfolding process, and we do it by practicing together. 
-          We consciously cultivate a post-conventional culture among us that shifts our seeing, 
-          supports our becoming, flows into our doing.</p>
-        <p>If you’ve been seeking the intersection of inner development and systems change, we 
+        <p><i>Being who we want to be</i> is an ever-unfolding process, and we do it by practicing together. 
+          We cultivate a post-conventional culture among us that shifts our seeing, 
+          supports our becoming, and grounds our doing.</p>
+        <p>If you’re looking for the intersection of inner development and systems change, we 
           invite you to join us in growth and friendship as we
-          practice awareness, acceptance, and integrity in a dance of transformation and service to life.</p>
+          practice <i>awareness, acceptance, integrity</i> in a dance of transformation and service to life.</p>
         <p>See how you can <Link to="/get-involved">get involved</Link>, or 
           learn more <Link to="/who-we-are">about us</Link>.</p>
-        </div>
 
       <IsHr />
       <p>Deeper inside IS, you'll find three spaces.</p>
       <div style={{ width: "fit-content", margin: "0 auto"}}>
       <img src="/images/s17-3spaces.jpg" loading="eager" width="480" alt="three spaces of IS: practice dojo, community hub, intentional ventures"/>
       </div>
-      <p>Our <Link to="/dojo">Practice Dojo</Link> is open to the public for developmental-relational skill-building.</p>
+      <p>The <Link to="/dojo">Practice Dojo</Link> is open to the public for developmental-relational skill-building.</p>
       <p>Our <Link to="/hub">Community Hub</Link> is a supportive space of friendship and exploration.</p>
-      <p><Link to="/iv">Intentional Ventures</Link> is pioneering an ecosystem of aligned livelihood</p>
-      <p>We'd also love to introduce you to our <Link to="/friends">friends</Link>, and align ourselves with 
-        the <a href="https://secondrenaissance.net">Second Renaissance</a> movement.</p>
+      <p><Link to="/iv">Intentional Ventures</Link> is pioneering an ecosystem of aligned livelihood.</p>
+      <p>We'd also love to introduce you to our <Link to="/friends">friends</Link> across  
+        the <a href="https://secondrenaissance.net">Second Renaissance</a> movement 
+        and the <a href="https://www.joelightfoot.org/post/the-liminal-web-mapping-an-emergent-subculture-of-sensemakers-meta-theorists-systems-poets">Liminal Web</a> scene.</p>
       {/*
       <Grid container spacing={2} justifyContent="center">
       {imageList.map((item, index) => (
@@ -120,12 +121,14 @@ const NamedDefault = ({ data }) => <>
     </Grid>
     */}
     <IsHr />
-      <p>The heart of IS has been beating for four years now, 
+      <p>The collective heart of IS has been beating for over four years now, 
         with <Link to="/history">more than 200 community sessions</Link> under our belts.</p>
       <p>Want to follow along with what we're doing and learning? 
-      Enter your email here to subscribe to our Weekly Update newsletter:</p>
+      Subscribe to our Weekly Update newsletter:</p>
       <ButtondownSignup></ButtondownSignup>
-      <p>You can also follow us <a href="https://twitter.com/IntentionalSoc">on Twitter</a>.</p>
+
+      </div>
+
       <div style={{textAlign: "right", marginBottom: "-25px"}}>
         Next page: <Link to="/who-we-are">Who We Are</Link>
       </div>
