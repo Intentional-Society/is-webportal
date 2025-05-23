ac7b0ec95d96592f8e4f3fe7c9f6f0b581c0eeb4
James Baker <james-baker@users.noreply.github.com>
2025-05-23 10:54:55 -0700

    updates

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,10 +1,7 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  Join us at <a href="https://www.limicon2025.com">Limicon 2025</a>! <br />
-  We're bringing the Practice Dojo into the convention and hosting topical fishbowl dialogues 
-  throughout the four-week span between March 5 to April 2. <br />
-  Regular IS Practice Dojo activities will resume Q2 2025.
+  The next Practice Dojo series is brewing now, stay tuned for more info soon!
   
   {/* Sessions are usually on <b>Saturdays from 
   1:00 to 2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. <a 
