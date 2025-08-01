7a2f72c76f12d1c8a2f85104aa4256c98fee77a3
James Baker <james-baker@users.noreply.github.com>
2025-07-31 21:31:30 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -2,9 +2,8 @@ import { Link } from "gatsby";
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  Coming up at the Practice Dojo: 
-  our <Link to="/developmental-practice-series">Developmental Practice Series</Link> starts 
-  Thursday, July 10!
+  Now running at the Practice Dojo: 
+  our <Link to="/developmental-practice-series">Developmental Practice Series</Link> July through September.
   
   {/* Sessions are usually on <b>Saturdays from 
   1:00 to 2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. <a 
