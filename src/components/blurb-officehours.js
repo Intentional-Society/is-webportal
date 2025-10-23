ea9cf603c34a33786e899bb5f044036e8c344f32
James Baker <james-baker@users.noreply.github.com>
2025-10-22 21:39:42 -0700

    that's a Saturday

--- src/components/blurb-officehours.js
+++ src/components/blurb-officehours.js
@@ -3,6 +3,6 @@ import React from "react"
 const NamedDefault = ({ data }) => <>
   <a href="https://us02web.zoom.us/meeting/register/tGlrmSgvSsOKPoAUyxm4aA">
   Click here to register</a> for our next IS Connection Call
-  on <b>Friday, November 1<sup>st</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>.
+  on <b>Saturday, November 1<sup>st</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>.
 </>
 export default NamedDefault;
