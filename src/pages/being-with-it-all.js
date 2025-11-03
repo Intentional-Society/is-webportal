86a08545b6e99a3cceb6281fb937a93d3d84c80f
James Baker <james-baker@users.noreply.github.com>
2025-11-02 23:22:30 -0800

    update for 2nd run

--- src/pages/being-with-it-all.js
+++ src/pages/being-with-it-all.js
@@ -7,7 +7,15 @@ const NamedDefault = () => {
   return (
     <Layout>
       <CenteredColumn>
-        <Typography variant="h4" style={{ textAlign: "left", margin: "25px 0" }}>Being With It All</Typography>
+        <Typography variant="h2">Being With It All</Typography>
+
+        <p>This is a practice series within the IS <Link to="/dojo">Practice Dojo</Link>.</p>
+
+        <p><b>November-December 2025:</b>Running now! Our next session 
+        is <b>Saturday, November 15<sup>th</sup> from 
+        1:00 to 2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. <a 
+        href="https://us02web.zoom.us/meeting/register/tZIvdOCtpj4qG9XxuaHMOq_7eGs_6dPCwhae">Click 
+        here to register and receive the meeting link.</a></p>
 
         <img
           src="/images/being-with-it-all.png"
@@ -24,11 +32,11 @@ const NamedDefault = () => {
           }}
         />
 
-        <p style={{ textAlign: "left" }}>
+        <p>
           Do you feel overwhelmed sometimes? Do you feel like you avoid bad news? Do you defend against being known and loved and rejected by keeping your distance? What would be different if you had more capacity to face it, and feel it without flinching away, and do what is right and good?
         </p>
         
-        <p style={{ textAlign: "left" }}>
+        <p>
           Being with it all is where we stretch our perspective-taking and hold space of okayness for it all.
         </p>
 
