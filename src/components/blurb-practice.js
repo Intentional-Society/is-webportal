7270293d1b08a0fbb68a4a8d97bed4808edaead0
James Baker <james-baker@users.noreply.github.com>
2025-11-02 23:17:11 -0800

    being with it all

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -2,11 +2,12 @@ import { Link } from "gatsby";
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next Practice Dojo series is "Being With It All", dates coming soon.
-  
-  {/* Sessions are usually on <b>Saturdays from 
+  The current Practice Dojo series is <Link to="/being-with-it-all">Being With It All</Link>.
+  All are welcome to this warm and heart-stretching practice space.
+  <br />
+  Our next session is <b>Saturday, November 15<sup>th</sup> from 
   1:00 to 2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. <a 
   href="https://us02web.zoom.us/meeting/register/tZIvdOCtpj4qG9XxuaHMOq_7eGs_6dPCwhae">Click 
-  here to register and receive the meeting link.</a> */}
+  here to register and receive the meeting link.</a>
 </>
 export default NamedDefault;
