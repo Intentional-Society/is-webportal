58446838d310a726c5e88f3f2e654769010bd3bb
James Baker <james-baker@users.noreply.github.com>
2025-11-02 23:48:48 -0800

    import Link

--- src/pages/being-with-it-all.js
+++ src/pages/being-with-it-all.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby"
 import Typography from '@material-ui/core/Typography';
 import Layout from "../components/layout";
 import CenteredColumn from "../components/centered-column";
