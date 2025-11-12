b8da9a636386ee410993ea3029f1770e93aaad33
James Baker <james-baker@users.noreply.github.com>
2025-11-11 20:22:31 -0800

    start branding page

---
+++ src/pages/branding.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+import Typography from '@material-ui/core/Typography';
+import IsHr from "../components/is-hr"
+import Layout from "../components/layout"
+import CenteredColumn from "../components/centered-column"
+import BlurbPractice from "../components/blurb-practice"
+import { StaticImage } from "gatsby-plugin-image"
+
+const NamedDefault = ({ data }) => (
+  <Layout>
+    <div style={{ height: "5vh" }}></div>
+    <CenteredColumn>
+      <Typography variant="h2">Branding</Typography>
+      <p>For press and partners including representations of Intentional Society in other media</p>
+      <p>Wordmark: <StaticImage src="../../static/images/branding/intentional-society-wordmark2024.png"/> </p>
+      <p>If higher-fidelity rendering is needed: This is Gudea font with a background color #24818E</p>
+    </CenteredColumn>
+    </Layout>
+);
+
