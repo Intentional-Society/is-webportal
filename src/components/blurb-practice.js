21c9a1b6f94b01a65543bf5178cca6d4e350694b
James Baker <james-baker@users.noreply.github.com>
2025-12-04 21:24:24 -0800

    final BWIA

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -5,9 +5,9 @@ const NamedDefault = ({ data }) => <>
   The current Practice Dojo series is <Link to="/being-with-it-all">Being With It All</Link>.
   All are welcome to this warm and heart-stretching practice space.
   <br />
-  Our next session is <b>Saturday, November 29<sup>th</sup> from 
+  Our next session is <b>Saturday, December 13<sup>th</sup> from 
   1:00 to 2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. <a 
-  href="https://us02web.zoom.us/meeting/register/CLB3xUoISkiA0bK3pfltKg">Click 
+  href="https://us02web.zoom.us/meeting/register/hBbSWtGnSKqmVHrUAnsq4g">Click 
   here to register and receive the meeting link.</a>
 </>
 export default NamedDefault;
