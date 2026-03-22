4fa80551d2e6255996ca5bebe3d435fe0d01146d
Ola <93337521+oolu4236@users.noreply.github.com>
2026-03-22 13:14:03 +0100

    Created RP section
    Relational Programs section condensed to a summary with a link to /programs

---
+++ src/pages/programs.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Layout from '../components/layout';
+import CenteredColumn from '../components/centered-column';
+import * as MarkdownStyles from '../styles/markdown-content.module.css';
+
+const NamedDefault = ({ data }) => (
+  <Layout>
+    <div style={{ height: '30px' }}></div>
+    <CenteredColumn>
+      <div
+        className={MarkdownStyles.markdownContent}
+        dangerouslySetInnerHTML={{__html: data.content.childMarkdownRemark.html}}
+      />
+      <div style={{textAlign: 'right', marginBottom: '-25px'}}>
+        Next page: <Link to="/web">Relational Web</Link>
+      </div>
+    </CenteredColumn>
+  </Layout>
+);
+
+export default NamedDefault;
+
+export const query = graphql`
+  query {
+    content: file(relativePath: { eq: "md/programs.md" }) {
+      childMarkdownRemark {
+        html
+      }
+    }
+  }
+`;
