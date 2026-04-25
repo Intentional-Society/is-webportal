bd5565173328f3c6783d6889fe7970605289e312
James Baker <james-baker@users.noreply.github.com>
2026-04-24 17:23:19 -0700

    Update Connection Call listing to May 8th 1pm Pacific

--- src/components/blurb-connectioncall.js
+++ src/components/blurb-connectioncall.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NamedDefault = ({ data }) => <>
-  Our next <a href="https://us02web.zoom.us/meeting/register/_UwuULWbQd6wEnOLCzrn1A">
-  IS Connection Call</a> is on <b>Friday, April 24<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>.
+  Our next <a href="https://us02web.zoom.us/meeting/register/4R6kC7FBQtiXl-z9ajmDPQ">
+  IS Connection Call</a> is on <b>Friday, May 8<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>.
 </>;
 export default NamedDefault;
